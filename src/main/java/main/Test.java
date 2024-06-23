963c8aa5d9c74ca99f473658bf47aaac00c50ecb
Christopher-OOC <olojedechristopher24@gmail.com>
2024-06-23 23:16:28 +0100

    message

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -1,8 +1,14 @@
 package main;
 
+import model.Car;
+
 public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("hello");
+		
+		Car car = new Car();
+		
+		System.out.println(car);
 	}
 }
