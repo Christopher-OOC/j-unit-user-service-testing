50437def1e5d5db3eaf41210951ac0442c49d762
Christopher-OOC <olojedechristopher24@gmail.com>
2024-06-27 12:08:21 +0100

    commmu

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -3,6 +3,11 @@ package main;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
 	}
 
 }
