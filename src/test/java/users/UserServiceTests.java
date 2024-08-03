35c796afd8da0bdeaa249f39537528ae034eb934
Christopher-OOC <olojedechristopher24@gmail.com>
2024-08-04 00:34:08 +0100

    commit

--- src/test/java/users/UserServiceTests.java
+++ src/test/java/users/UserServiceTests.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import main.User;
 import main.UserService;
