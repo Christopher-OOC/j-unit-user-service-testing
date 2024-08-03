06fe6f53ddf84460da508455855beac2f20fc420
Christopher-OOC <olojedechristopher24@gmail.com>
2024-08-03 18:30:07 +0100

    commit

---
+++ src/test/java/users/UserServiceTests.java
@@ -0,0 +1,20 @@
+package users;
+
+import org.junit.jupiter.api.Test;
+
+import main.UserService;
+import main.UserServiceImpl;
+
+public class UserServiceTests {
+	
+	@Test
+	void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
+		// Arrange
+		UserService userService = new UserServiceImpl();
+		
+		//Act
+		
+		//Assert 
+	}
+
+}
