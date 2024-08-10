3528e7627580d4b59f566be900a8f409b4905deb
Christopher-OOC <olojedechristopher24@gmail.com>
2024-08-10 07:20:52 +0100

    commit

--- src/main/java/main/EmailVerificationServiceImpl.java
+++ src/main/java/main/EmailVerificationServiceImpl.java
@@ -7,6 +7,8 @@ public class EmailVerificationServiceImpl implements EmailVerificationService {
 		System.out.println("Hello");
 		System.out.println("Hello");
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 	}
 
 }
