f921936e544fe98199547765a44ffe8832e1c2eb
Christopher-OOC <olojedechristopher24@gmail.com>
2024-08-10 07:20:29 +0100

    commit

--- src/main/java/main/EmailVerificationServiceImpl.java
+++ src/main/java/main/EmailVerificationServiceImpl.java
@@ -5,6 +5,8 @@ public class EmailVerificationServiceImpl implements EmailVerificationService {
 	@Override
 	public void scheduleEmailConfirmation(User user) {
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 	}
 
 }
